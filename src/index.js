ae0098da3e9c7efe51a8b99d0805db328d78d758
eugeniakhaleeva <khaleeva20990@gmail.com>
2023-01-16 19:29:54 +0100

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,21 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = expr.split('');
+    const groups = [];
+    while (arr.length > 0) {
+        groups.push(arr.splice(0, 10));
+    }
+    let newGroups = groups.map(i => i.join('')).map(i => i.replace(/^0+/, ""))
+
+    const regex1 = /10/ig, regex2 = /11/ig
+
+    let str = newGroups.map(i => i.replace(regex1, '.'))
+        .map(a => a.replace(regex2, '-'))
+
+    return str.map(i => MORSE_TABLE[i] ? MORSE_TABLE[i] : ' ').join('')
+
+
 }
 
 module.exports = {
